11b811e05ccea3256311d82b53e26d1bcf2f80d9
AleManni <ale_manni@mail.com>
2021-04-06 15:44:06 +0100

    Initial Commit

---
+++ CombineTest/CombineTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineTestApp.swift
+//  CombineTest
+//
+//  Created by Alessandro Manni on 06/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct CombineTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
