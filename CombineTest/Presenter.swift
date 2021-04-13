064edd28f4be791a58762f414b5bd9e44b36b7bb
AleManni <ale_manni@mail.com>
2021-04-13 22:21:10 +0100

    pattern 2

--- CombineTest/Presenter.swift
+++ CombineTest/Presenter.swift
@@ -52,25 +52,6 @@ class Presenter: ObservableObject {
       .store(in: &cancellables)
   }
   
-  // ALTERNATIVE PATTERN USING CombineLatest
-  func start2()  {
-    useCaseOne.start()
-    useCaseTwo.start()
-    
-    Publishers
-      .CombineLatest(useCaseOne.caseState.receive(on: DispatchQueue.main),
-                     useCaseTwo.caseState.receive(on: DispatchQueue.main))
-      .sink(receiveCompletion: { [weak self] error in
-        self?.onCompletion?(error)
-      },
-      receiveValue: { [weak self] state1, state2 in
-        self?.state1 = state1
-        self?.state2 = state2
-        self?.makeViewModel()
-      })
-      .store(in: &cancellables)
-  }
-  
   private func makeViewModel() {
     switch (state1, state2, receivedError) {
       case (_, _, let error) where error != nil:
@@ -82,5 +63,51 @@ class Presenter: ObservableObject {
     default:
       break
     }
+  }
+  
+  // ALTERNATIVE PATTERN USING CombineLatest and StateHandler
+  
+  private lazy var actions = StatesHandler.Actions<DomainModelOne, DomainModelTwo>(
+    onLoading: {
+      self.compositeModel = ViewModelItem(name: "Loading ⌛")
+    },
+    onSuccess: { model1, model2 in
+      self.compositeModel = ViewModelItem(name: "🎉 1️⃣ \(model1.name) 2️⃣ \(String(model2.number))")
+    },
+    onError: { _ in
+      self.compositeModel = ViewModelItem(name: "Failure ❌")
+    })
+  
+  lazy var onCompletion2: ((Subscribers.Completion<Error>) -> Void)? = { [weak self] completion in
+      switch completion {
+      case .finished:
+       break
+      case .failure(let error):
+        guard let self = self else { return }
+        StatesHandler.handle(state1: nil,
+                             state2: nil,
+                             error: error,
+                             actions: self.actions)
+      }
+  }
+  
+  func start2()  {
+    useCaseOne.start()
+    useCaseTwo.start()
+    
+    Publishers
+      .CombineLatest(useCaseOne.caseState.receive(on: DispatchQueue.main),
+                     useCaseTwo.caseState.receive(on: DispatchQueue.main))
+      .sink(receiveCompletion: { [weak self] error in
+        self?.onCompletion2?(error)
+      },
+      receiveValue: { [weak self] state1, state2 in
+        guard let self = self else { return }
+        StatesHandler.handle(state1: state1,
+                             state2: state2,
+                             error: nil,
+                             actions: self.actions)
+      })
+      .store(in: &cancellables)
   }
 }
